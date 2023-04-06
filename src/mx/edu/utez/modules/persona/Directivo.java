c58db943efdc0e7a912357d89a900accd21d27af
Leonardo Noriega <leonoriega100@gmail.com>
2023-04-06 09:57:52 -0600

    CRUD agregado clase Directivo

--- src/mx/edu/utez/modules/persona/Directivo.java
+++ src/mx/edu/utez/modules/persona/Directivo.java
@@ -1,11 +1,16 @@
 package mx.edu.utez.modules.persona;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Directivo extends Persona {
     private String email;
     private String ubicacion;
     // @TODO: En el diagrama de clases cambiar atributo "tipo" por "puesto"
     private String puesto;
     private double sueldo;
+    private List<Alumno> alumnos = new ArrayList<>();
+    private List<Profesor> profesores = new ArrayList<>();
 
     public Directivo() {
     }
@@ -61,28 +66,89 @@ public class Directivo extends Persona {
      * -- Cambiar diagrama de clases con estos nuevos metodos --
      * Crud de alumnos y profesores
      */
-    public void consultarAlumno() {
+    public List<Alumno> imprimirAlumnos() {
+        return alumnos;
     }
 
-    public void darAltaAlumno() {
+    public void consultarAlumnos(String nombre) {
+        for (Alumno alumno : alumnos) {
+            if (alumno.getNombre().equals(nombre)) {
+                System.out.println(alumno);
+                return;
+            }
+        }
+        System.out.println("No se encontró al alumno con el nombre proporcionado.");
     }
 
-    public void actualizarAlumno() {
+    public void darAltaAlumno(Alumno alumno) {
+        alumnos.add(alumno);
+        System.out.println("Se ha dado de alta al alumno exitosamente.");
     }
 
-    public void darBajaAlumno() {
+    public void actualizarAlumno(String nombre, Alumno alumno) {
+        for (int i = 0; i < alumnos.size(); i++) {
+            Alumno alm = alumnos.get(i);
+            if (alm.getNombre().equals(nombre)) {
+                alumnos.set(i, alumno);
+                System.out.println("Se ha actualizado al alumno exitosamente.");
+                return;
+            }
+        }
+        System.out.println("No se encontró al alumno con la matricula proporcionada.");
     }
 
-    public void consultarProfesor() {
+    public void darBajaAlumno(String matricula) {
+        for (int i = 0; i < alumnos.size(); i++) {
+            Alumno alumno = alumnos.get(i);
+            if (alumno.getMatricula().equals(matricula)) {
+                alumnos.remove(i);
+                System.out.println("Se ha dado de baja al alumno exitosamente.");
+                return;
+            }
+        }
     }
 
-    public void darAltaProfesor() {
+    public List<Profesor> imprimirProfesores() {
+        return profesores;
     }
 
-    public void actualizarProfesor() {
+    public void consultarProfesor(String nombre) {
+        for (Profesor profesor : profesores) {
+            if (profesor.getNombre().equals(nombre)) {
+                System.out.println(profesor);
+                return;
+            }
+        }
+        System.out.println("No se encontró al profesor con el nombre proporcionado.");
     }
 
-    public void darBajaProfesor() {
+    public void darAltaProfesor(Profesor profesor) {
+        profesores.add(profesor);
+        System.out.println("Se ha dado de alta al profesor exitosamente.");
+    }
+
+    public void actualizarProfesor(String nombre, Profesor profesor) {
+        for (int i = 0; i < profesores.size(); i++) {
+            Profesor prf = profesores.get(i);
+            if (prf.getNombre().equals(nombre)) {
+                profesores.set(i, profesor);
+                System.out.println("Se ha actualizado al profesor exitosamente.");
+                return;
+            }
+        }
+        System.out.println("No se encontró al profesor con el nombre proporcionado.");
+    }
+
+    public void darBajaProfesor(String nombre) {
+        for (int i = 0; i < profesores.size(); i++) {
+            Profesor profesor = profesores.get(i);
+            if (profesor.getNombre().equals(nombre)) {
+                profesores.remove(i);
+                System.out.println("Se ha dado de baja al profesor exitosamente.");
+                return;
+            }
+        }
+        System.out.println("No se encontró al profesor con el nombre proporcionado.");
     }
 
     // @TODO: Rellenar método de la clase con lo que debe hacer
