51fc000cc0d1b7d93dc831d6c3cefd9d3f7af6d4
Leo-Noriega <leonoriega100@gmail.com>
2023-04-17 16:23:25 -0600

    Distribución de proyecto actualizada

--- src/mx/edu/utez/Main.java
+++ src/mx/edu/utez/Main.java
@@ -7,80 +7,207 @@ import mx.edu.utez.modules.persona.*;
 public class Main {
 
     public static void main(String[] args) {
-        // Menu de para pedir credenciales
         Scanner sc = new Scanner(System.in);
-        System.out.println("* BIENVENIDO AL SISTEMA *\nPor favor, indique si es 'estudiante' o 'directivo': ");
-        String tipoUsuario = sc.nextLine().toLowerCase();
-        while (!tipoUsuario.equals("estudiante") && !tipoUsuario.equals("directivo")) {
-            System.out.println(
-                    "Tipo de usuario incorrecto. Por favor, indique si es 'estudiante' o 'directivo': ");
+        String tipoUsuario;
+
+        do {
+            // Menu de para pedir credenciales
+            System.out.print("* BIENVENIDO AL SISTEMA *"
+                    + "\nPor favor, indique si es 'estudiante', 'directivo' o 'Profesor', "
+                    + "\nSi desea salir del programa en cualquier momento, escriba salir: ");
             tipoUsuario = sc.nextLine().toLowerCase();
-        }
-        // Definiendo flag para continuar todo el programa
-        boolean continuar = true;
-        // Creaación de credenciales staticas predeterminadas
-        Credenciales credencial = new Credenciales();
-        // Llamar metodo para validar credenciales segun corresponda
-        if (tipoUsuario.equals("estudiante")) {
-            credencial.validarCreedencialesEstu();
-        }
-        if (tipoUsuario.equals("directivo")) {
-            credencial.validarCreedencialesDir();
-            if (credencial.getUsuarioDir().equals("pedrorector")) {
-                // Creando directivo para sus métodos
-                Directivo directivo = new Directivo("Pedro", "Ortiz", "Gonzalez", 'H', 45,
-                        "Calle Revolución #2",
-                        "Mexicano",
-                        "555-123-4567", "pedroortiz@utez.edu.mx", "Edificio A, piso 2",
-                        "Rector",
-                        100000);
-                System.out.println("Bienvenido " + directivo.getNombre() + " "
-                        + directivo.getApellidoPaterno() + " "
-                        + directivo.getApellidoMaterno() + "");
-                // Creando alumnos predeterminados para sus métodos (5)
-                Alumno alumno1 = new Alumno("Juan", "Pérez", "García", 'M', 18, "Calle 123", "Mexicana",
-                        "1234567890",
-                        "2023tn01", "juanperez@utez.edu.mx", new String[]{"9", "10", "8"},
-                        'A');
 
-                Alumno alumno2 = new Alumno("María", "Gómez", "Hernández", 'F', 20, "Calle 456",
-                        "Mexicana",
-                        "0987654321",
-                        "2023tn02", "mariagomez@utez.edu.mx", new String[]{"8", "7", "9"},
-                        'B');
+            while (!tipoUsuario.equals("estudiante")
+                    && !tipoUsuario.equals("directivo")
+                    && !tipoUsuario.equals("profesor")
+                    && !tipoUsuario.equals("salir")) {
 
-                Alumno alumno3 = new Alumno("Carlos", "Sánchez", "López", 'M', 22, "Calle 789",
-                        "Mexicana",
-                        "1357902468",
-                        "2023tn03", "carlossanchez@utez.edu.mx",
-                        new String[]{"10", "10", "10"},
-                        'C');
-
-                Alumno alumno4 = new Alumno("Ana", "Martínez", "Flores", 'F', 19, "Calle 321",
-                        "Mexicana",
-                        "2468013579",
-                        "2023tn04", "anamartinez@utez.edu.mx", new String[]{"7", "8", "6"},
-                        'A');
-
-                Alumno alumno5 = new Alumno("Luis", "Hernández", "Castillo", 'M', 21, "Calle 654",
-                        "Mexicana",
-                        "3692581470",
-                        "2023tn05", "luishernandez@utez.edu.mx", new String[]{"9", "7", "8"},
-                        'B');
+                System.out.print("Tipo de usuario incorrecto. "
+                        + "Por favor, indique si es 'estudiante', 'directivo' o 'Profesor':  ");
+                tipoUsuario = sc.nextLine().toLowerCase();
             }
-            if (credencial.getUsuarioDir().equals("xiomaradatid")) {
-                // Creando directivo para sus métodos
-                Directivo directivo = new Directivo("Xiomara", "Benitez", "Martinez", 'M', 42,
-                        "Calle Alta Tensión #99",
-                        "Mexicana",
-                        "555-987-6543", "xiomarabenitez@utez.edu.mx", "Edificio B, piso 1",
-                        "Directora DATID",
-                        90000);
-                System.out.println("Bienvenido " + directivo.getNombre() + " "
-                        + directivo.getApellidoPaterno() + " " + directivo.getApellidoMaterno()
-                        + "");
+
+            // Creación de credenciales staticas predeterminadas
+            Credenciales credencial = new Credenciales();
+
+            // Llamar metodo para validar credenciales segun corresponda
+            if (tipoUsuario.equals("estudiante")) {
+                credencial.validarCreedencialesEstu();
+                System.out.println("BIENVENIDO: "
+                        + "\n¿Qué deseas hacer?"
+                        + "\n1.- Solicitar baja"
+                        + "\n2.- Mirar calificaciones");
+                int decisionA = sc.nextInt();
+                sc.nextLine();
+                switch (decisionA) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
             }
-        }
+            if (tipoUsuario.equals("directivo")) {
+                credencial.validarCreedencialesDir();
+
+                if (credencial.getUsuarioDir().equals("pedrorector")) {
+                    // Creando directivo para sus métodos
+                    Directivo directivo = new Directivo("Pedro", "Ortiz", "Gonzalez", 'H', 45,
+                            "Calle Revolución #2",
+                            "Mexicano",
+                            "555-123-4567", "pedroortiz@utez.edu.mx", "Edificio A, piso 2",
+                            "Rector",
+                            100000);
+                    System.out.println("Bienvenido " + directivo.getNombre() + " "
+                            + directivo.getApellidoPaterno() + " "
+                            + directivo.getApellidoMaterno() + "");
+
+                    System.out.println("¿Qupe acción deseas realizar?"
+                            + "\n1.- Dar altas"
+                            + "\n2.- Consultar"
+                            + "\n3.- Actualizar Información "
+                            + "\n4.- Dar bajas"
+                            + "\n5.- Salir");
+                    int decision = sc.nextInt();
+                    sc.nextLine();
+
+                    switch (decision) {
+                        case 1:
+                            System.out.println("¿Qué persona deseas dar de alta?"
+                                    + "\n1.- Alumno"
+                                    + "\n2.- Profesor"
+                                    + "\n3.- Personal"
+                                    + "\n5.- Salir");
+                            int decision2 = sc.nextInt();
+                            switch (decision2) {
+                                case 1:
+
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                                default:
+                                    throw new AssertionError();
+                            }
+
+                            break;
+                        case 2:
+                            System.out.println("¿Qué persona deseas Consultar?"
+                                    + "\n1.- Alumno"
+                                    + "\n2.- Profesor"
+                                    + "\n3.- Personal"
+                                    + "\n5.- Salir");
+                            int decision3 = sc.nextInt();
+                            switch (decision3) {
+                                case 1:
+
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                                default:
+                                    throw new AssertionError();
+                            }
+
+                            break;
+                        case 3:
+                            System.out.println("¿Qué persona deseas Actualizar?"
+                                    + "\n1.- Alumno"
+                                    + "\n2.- Profesor"
+                                    + "\n3.- Personal"
+                                    + "\n4.- Salir");
+                            int decision4 = sc.nextInt();
+                            switch (decision4) {
+                                case 1:
+
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                                default:
+                                    throw new AssertionError();
+                            }
+                            break;
+                        case 4:
+                            System.out.println("¿Qué persona deseas dar de baja?"
+                                    + "\n1.- Alumno"
+                                    + "\n2.- Profesor"
+                                    + "\n3.- Personal"
+                                    + "\n5.- Salir");
+                            int decision5 = sc.nextInt();
+                            switch (decision5) {
+                                case 1:
+
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                                default:
+                                    throw new AssertionError();
+                            }
+                            break;
+                        case 5:
+                            System.out.println("Gracias por usar el sistema");
+                        default:
+                            throw new AssertionError();
+                    }
+                }
+            }
+
+            if (tipoUsuario.equals("profesor")) {
+                credencial.validarCreedencialesProf();
+                if (credencial.getUsuarioProf().equals("leonardonoriega")) {
+
+                }
+            }
+
+            // Creando alumnos predeterminados para sus métodos (5)
+            Alumno alumno1 = new Alumno("Juan", "Pérez", "García", 'M', 18, "Calle 123", "Mexicana",
+                    "1234567890",
+                    "2023tn01", "juanperez@utez.edu.mx", new String[]{"9", "10", "8"},
+                    'A');
+
+            Alumno alumno2 = new Alumno("María", "Gómez", "Hernández", 'F', 20, "Calle 456",
+                    "Mexicana",
+                    "0987654321",
+                    "2023tn02", "mariagomez@utez.edu.mx", new String[]{"8", "7", "9"},
+                    'B');
+
+            Alumno alumno3 = new Alumno("Carlos", "Sánchez", "López", 'M', 22, "Calle 789",
+                    "Mexicana",
+                    "1357902468",
+                    "2023tn03", "carlossanchez@utez.edu.mx",
+                    new String[]{"10", "10", "10"},
+                    'C');
+
+            Alumno alumno4 = new Alumno("Ana", "Martínez", "Flores", 'F', 19, "Calle 321",
+                    "Mexicana",
+                    "2468013579",
+                    "2023tn04", "anamartinez@utez.edu.mx", new String[]{"7", "8", "6"},
+                    'A');
+
+            Alumno alumno5 = new Alumno("Luis", "Hernández", "Castillo", 'M', 21, "Calle 654",
+                    "Mexicana",
+                    "3692581470",
+                    "2023tn05", "luishernandez@utez.edu.mx", new String[]{"9", "7", "8"},
+                    'B');
+
+        } while (!tipoUsuario.equals("salir"));
     }
 
 }
--- src/mx/edu/utez/herramientas/Credenciales.java
+++ src/mx/edu/utez/herramientas/Credenciales.java
@@ -3,37 +3,49 @@ package mx.edu.utez.herramientas;
 import java.util.Scanner;
 
 //@TODO: Añadir esta clase al diagrama
-
 public class Credenciales {
+
     private String[] usuariosEstu;
     private String[] contrasenasEstu;
     private String[] usuariosDir;
     private String[] contrasenasDir;
+    private String[] usuariosProf;
+    private String[] contrasenasProf;
     private boolean credencialesValidas;
     private int posUsuario;
 
     public Credenciales() {
-        this.usuariosEstu = new String[] { "2023tn01", "2023tn02", "2023tn03", "2023tn04", "2023tn05" };
-        this.contrasenasEstu = new String[] { "juan01", "maria02", "carlos03", "ana04", "luis05" };
-        this.usuariosDir = new String[] { "pedrorector", "xiomaradatid" };
-        this.contrasenasDir = new String[] { "pedroortiza2", "xiomarabenitezb1" };
+        this.usuariosEstu = new String[]{"2023tn01", "2023tn02", "2023tn03", "2023tn04", "2023tn05"};
+        this.contrasenasEstu = new String[]{"juan01", "maria02", "carlos03", "ana04", "luis05"};
+        this.usuariosDir = new String[]{"pedrorector", "xiomaradatid"};
+        this.contrasenasDir = new String[]{"pedroortiza2", "xiomarabenitezb1"};
+        this.usuariosProf = new String[]{"juanlopez", "leonardonoriega"};
+        this.contrasenasProf = new String[]{"juanlopez12", "leonardonoriega2"};
         this.credencialesValidas = false;
-        this.posUsuario = 0;
-    }
-
-    public boolean getCredencialesValidas() {
-        return credencialesValidas;
+        this.posUsuario = -1;
     }
 
     public String getUsuarioEstu() {
         return usuariosEstu[posUsuario];
     }
 
+    public int getPosUsuarioEstu() {
+        return posUsuario;
+    }
+
     public String getUsuarioDir() {
         return usuariosDir[posUsuario];
     }
 
-    public int getPosUsuario() {
+    public int getPosUsuarioDir() {
+        return posUsuario;
+    }
+
+    public String getUsuarioProf() {
+        return usuariosProf[posUsuario];
+    }
+
+    public int getPosUsuarioProf() {
         return posUsuario;
     }
 
@@ -70,6 +82,27 @@ public class Credenciales {
                     credencialesValidas = true;
                     posUsuario = i;
                     System.out.println("Has iniciado sesión correctamente " + usuariosDir[posUsuario]);
+                    break;
+                } else {
+                    System.out.println("Usuario o contraseña incorrectos");
+                }
+                break;
+            }
+        }
+    }
+
+    public void validarCreedencialesProf() {
+        Scanner sc = new Scanner(System.in);
+        while (!credencialesValidas) {
+            System.out.println("Ingrese su usuario: ");
+            String usuario = sc.nextLine();
+            System.out.println("Ingrese su contraseña: ");
+            String contrasena = sc.nextLine();
+            for (int i = 0; i < usuariosProf.length;) {
+                if (usuario.equals(usuariosProf[i]) && contrasena.equals(contrasenasProf[i])) {
+                    credencialesValidas = true;
+                    posUsuario = i;
+                    System.out.println("Has iniciado sesión correctamente " + usuariosProf[posUsuario]);
                     break;
                 } else {
                     System.out.println("Usuario o contraseña incorrectos");
