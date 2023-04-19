ab0b77c99165aafdf1280342f30326d64e214e88
Leo-Noriega <leonoriega100@gmail.com>
2023-04-18 21:57:55 -0600

    Funcionalidades añadidas

--- src/mx/edu/utez/herramientas/Archivo.java
+++ src/mx/edu/utez/herramientas/Archivo.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package mx.edu.utez.herramientas;
 
-/**
- *
- * @author leonardonoriega
- */
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Archivo {
-    
+
+    public static void escribir(String nombreArchivo, String[] mensajes) {
+        try {
+            FileWriter escritor = new FileWriter(nombreArchivo + ".txt");
+            //
+            for (String mensaje : mensajes) {
+                escritor.write(mensaje + "\n");
+            }
+            escritor.close();
+            System.out.println("Se ha creado el archivo " + nombreArchivo + " correctamente");
+        } catch (IOException e) {
+            System.out.println("Ocurrio un error");
+            e.printStackTrace();
+        }
+    }
+
+    public static void escribir(String nombreArchivo, String mensaje) {
+        try {
+            FileWriter escritor = new FileWriter(nombreArchivo + ".txt");
+            escritor.write(mensaje + "\n");
+            escritor.close();
+            System.out.println("Se ha creado el archivo " + nombreArchivo + " correctamente");
+        } catch (IOException e) {
+            System.out.println("Ocurrio un error");
+            e.printStackTrace();
+        }
+    }
 }
