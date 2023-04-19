4883eec53e447b3b7e155d2d31df8357ff4def24
Leo-Noriega <leonoriega100@gmail.com>
2023-04-19 03:27:58 -0600

    Métodos añadidos para Alumno

--- src/mx/edu/utez/herramientas/Archivo.java
+++ src/mx/edu/utez/herramientas/Archivo.java
@@ -6,21 +6,6 @@ import mx.edu.utez.modules.persona.Alumno;
 
 public class Archivo {
 
-    public static void escribir(String nombreArchivo, String[] mensajes) {
-        try {
-            FileWriter escritor = new FileWriter(nombreArchivo + ".txt");
-            //
-            for (String mensaje : mensajes) {
-                escritor.write(mensaje + "\n");
-            }
-            escritor.close();
-            System.out.println("Se ha creado el archivo " + nombreArchivo + " correctamente");
-        } catch (IOException e) {
-            System.out.println("Ocurrio un error");
-            e.printStackTrace();
-        }
-    }
-
     public static void mensajeAlta(String nombreArchivo, Alumno alumno) {
         try {
             FileWriter escritor = new FileWriter(nombreArchivo + ".txt");
--- src/mx/edu/utez/modules/persona/Alumno.java
+++ src/mx/edu/utez/modules/persona/Alumno.java
@@ -1,10 +1,12 @@
 package mx.edu.utez.modules.persona;
 
+import java.util.Random;
+
 public class Alumno extends Persona {
 
     private String matricula;
     private String correoInstitucional;
-    // @TODO: Modificar en el diagrama de classses Calificaciones[] por un String[]
+    // @TODO: Modificar en el diagrama de classses Calificaciones[] por un String[]
     private String[] calificaciones;
     // @TODO: Modificar en el diagrama de clases String a char y borrar "Horario"
     private char grado;
@@ -69,16 +71,45 @@ public class Alumno extends Persona {
      * Hacer Tareas
      * Entregar Tareas
      */
-    public String[] consultarCalificaciones() {
-        return calificaciones;
-    }
-
     @Override
     public String toString() {
-        return "Alumno: " + nombre + " " + apellidoPaterno + " " + apellidoMaterno
-                + "\nEdad: " + edad + "\t| Dirección: " + direccion + "\t| Telefono: " + telefono
-                + "\nMatricula: " + matricula + "\t| Correo Institucional: " + correoInstitucional
-                + "\nGrado: " + grado + "\n";
+        return "Alumno{" + "matricula=" + matricula + ", correoInstitucional=" + correoInstitucional + ", calificaciones=" + calificaciones + ", grado=" + grado + '}';
     }
 
+    public void consultarCalificaciones() {
+        String[][] arreglo = new String[7][6];
+        Random rand = new Random();
+        String[] calificaciones = {"  SA    ", "  DE    ", "  AU    "};
+
+        arreglo[0][0] = "  Materias  ";
+        arreglo[0][1] = "Unidad 1";
+        arreglo[0][2] = "Unidad 2";
+        arreglo[0][3] = "Unidad 3";
+        arreglo[0][4] = "Unidad 4";
+        arreglo[0][5] = "Unidad 5";
+        arreglo[1][0] = "Programación";
+        arreglo[2][0] = "Matematicas ";
+        arreglo[3][0] = "Español     ";
+        arreglo[4][0] = "Ingles      ";
+        arreglo[5][0] = "Química     ";
+        arreglo[6][0] = "Física      ";
+
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                if ((i != 0 || j != 0) && (i != 0 || j != 1)
+                        && (i != 0 || j != 2) && (i != 0 || j != 3)
+                        && (i != 0 || j != 4) && (i != 0 || j != 5)
+                        && (i != 1 || j != 0)
+                        && (i != 2 || j != 0) && (i != 3 || j != 0)
+                        && (i != 4 || j != 0) && (i != 5 || j != 0)
+                        && (i != 6 || j != 0)) {
+                    int indiceAleatorio = rand.nextInt(calificaciones.length);
+                    arreglo[i][j] = calificaciones[indiceAleatorio];
+                }
+                System.out.print("| " + arreglo[i][j] + " ");
+            }
+            System.out.println();
+        }
+        //return calificaciones;
+    }
 }
